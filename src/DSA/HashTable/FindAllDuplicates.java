6de84efb1c91b24b2c3b8b33f98da0fd986ffe9d
404FoundU <unnikrishnanrp@outlook.com>
2024-06-20 11:01:15 -0500

    Added Map loop

---
+++ src/DSA/HashTable/FindAllDuplicates.java
@@ -0,0 +1,40 @@
+package DSA.HashTable;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FindAllDuplicates {
+
+
+    public static void main(String[] args) {
+        FindAllDuplicates test = new FindAllDuplicates();
+        int[] arr1 = {2, 5, 8, 9, 4, 2, 5};
+        List<Integer> integers = test.testMethod(arr1);
+        System.out.println(integers);
+    }
+
+    private List<Integer> testMethod(int[] arr1) {
+        Map<Integer, Integer> hs = new HashMap<>();
+        for (int j : arr1) {
+            if (hs.get(j) == null) {
+                hs.put(j, 1);
+            } else {
+                Integer count = hs.get(j);
+                hs.put(j, count + 1);
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : hs.entrySet()) {
+            Integer k = entry.getKey();
+            Integer v = entry.getValue();
+            if (v > 1) {
+                list.add(k);
+            }
+        }
+
+        return list;
+    }
+}
