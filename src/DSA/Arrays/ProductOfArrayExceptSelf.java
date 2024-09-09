a631e3d42858247b899ad617554b44306d4b0908
404FoundU <unnikrishnanrp@outlook.com>
2024-09-09 10:12:30 -0500

    array

--- src/DSA/Arrays/ProductOfArrayExceptSelf.java
+++ src/DSA/Arrays/ProductOfArrayExceptSelf.java
@@ -8,20 +8,19 @@ public class ProductOfArrayExceptSelf {
     public static void main(String[] args) {
         ProductOfArrayExceptSelf productOfArrayExceptSelf = new ProductOfArrayExceptSelf();
         int[] nums = {1, -2, 3, -4, 5};
-        productOfArrayExceptSelf.test(nums);
+        productOfArrayExceptSelf.prefixsuffix(nums);
+        productOfArrayExceptSelf.singleArr(nums);
 
     }
 
-    private int test(int[] nums) {
+    private int prefixsuffix(int[] nums) {
 
         int[] arr = new int[nums.length];
         int right = 1;
         int left = 1;
         for (int i = nums.length - 1; i >= 0; i--) {
-
             arr[i] = right;
             right = nums[i] * right;
-
         }
         System.out.println("arr = " + Arrays.toString(arr));
 
@@ -30,7 +29,24 @@ public class ProductOfArrayExceptSelf {
             left = left * nums[j];
         }
         System.out.println("arr = " + Arrays.toString(arr));
-
         return 0;
     }
+
+    private int singleArr(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[n];
+        Arrays.fill(arr, 1);
+        int left = 1;
+        int right = 1;
+        for (int i = 0; i < n; i++) {
+            arr[i] = arr[i] * left;
+            left = left * nums[i];
+            arr[n - i - 1] *= right;
+            right = right * nums[n - i - 1];
+        }
+        System.out.println("arr = " + Arrays.toString(arr));
+        return 0;
+    }
+
+
 }
