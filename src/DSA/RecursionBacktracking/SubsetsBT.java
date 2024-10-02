0f71e3bc983a6f1e82a177f1aecc3370670ccab5
404FoundU <unnikrishnanrp@outlook.com>
2024-10-01 21:29:27 -0500

    subsets

--- src/DSA/RecursionBacktracking/SubsetsBT.java
+++ src/DSA/RecursionBacktracking/SubsetsBT.java
@@ -7,25 +7,25 @@ import java.util.List;
 public class SubsetsBT {
 
     static List<List<Integer>> result = new ArrayList<>();
+    static List<Integer> sol = new ArrayList<>();
+    static List<Integer> nums = new ArrayList<>();
 
-    public static List<List<Integer>> subsets(List<Integer> nums) {
-        List<Integer> sol = new ArrayList<>();
-
-        backtrack(nums, sol, 0);
-
+    public static List<List<Integer>> subsets(List<Integer> arr) {
+        nums.addAll(arr);
+        backtrack(0);
         return result;
     }
 
-    private static void backtrack(List<Integer> nums, List<Integer> sol, int start) {
+    private static void backtrack(int start) {
         if (nums.size() == start) { // index goes out of bound
             result.add(new ArrayList<>(sol));
             return;
         }
         //Dont pick nums[i]
-        backtrack(nums, sol, start + 1);
+        backtrack(start + 1);
         //pick nums[i]
         sol.add(nums.get(start));
-        backtrack(nums, sol, start + 1);
+        backtrack(start + 1);
         sol.remove(sol.size() - 1);
     }
 
