1a19965879dcc9a8ed78c9e551000526d85b6192
404FoundU <unnikrishnanrp@outlook.com>
2024-12-09 20:24:33 -0600

    serializable

---
+++ src/Snippets/Serializable/SerializationExample.java
@@ -0,0 +1,30 @@
+package Snippets.Serializable;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class SerializationExample {
+    public static void main(String[] args) {
+        Person person = new Person("John Doe", 30);
+
+        // Serialize the object
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("person.ser"))) {
+            oos.writeObject(person);
+            System.out.println("Object has been serialized: " + person);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Deserialize the object
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("person.ser"))) {
+            Person deserializedPerson = (Person) ois.readObject();
+            System.out.println("Object has been deserialized: " + deserializedPerson);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
