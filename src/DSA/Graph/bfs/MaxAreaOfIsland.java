5854c5ee55657aaf9525783a4e4120f8661f40c7
404FoundU <unnikrishnanrp@outlook.com>
2024-12-19 13:50:59 -0600

    graph

--- src/DSA/Graph/bfs/MaxAreaOfIsland.java
+++ src/DSA/Graph/bfs/MaxAreaOfIsland.java
@@ -12,49 +12,51 @@ public class MaxAreaOfIsland {
     };
 
     public static int maxAreaOfIsland(int[][] grid) {
-        if (grid == null || grid.length == 0) return 0;
-
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
         int maxArea = 0;
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                // Start a BFS when a new island (1) is found
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    int newArea = bfs(grid, i, j);
-                    maxArea = Math.max(maxArea, newArea);
+                    int area = bfs(grid, i, j);
+                    maxArea = Math.max(area, maxArea);
+
                 }
             }
         }
-
         return maxArea;
     }
 
     // BFS to calculate area of the island starting from (i, j)
     private static int bfs(int[][] grid, int i, int j) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{i, j});
-        grid[i][j] = 0; // Mark as visited by setting it to '0'
-        int area = 1; // Initial area for the current land cell
 
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
+        int m = grid.length;
+        int n = grid[0].length;
+        int[] source = new int[]{i, j};
+        Queue<int[]> q = new LinkedList<>();
+        q.add(source);
+        grid[i][j] = 0;
+        int area = 1;
+        while (!q.isEmpty()) {
+            int[] current = q.poll();
             int row = current[0];
             int col = current[1];
-
-            // Check all 4 possible directions (up, down, left, right)
             for (int[] direction : DIRECTIONS) {
                 int r = row + direction[0];
                 int c = col + direction[1];
 
-                // Check if the new position is within bounds and is land ('1')
-                if (r >= 0 && r < grid.length && c >= 0 && c < grid[0].length && grid[r][c] == 1) {
-                    queue.add(new int[]{r, c});
-                    grid[r][c] = 0; // Mark as visited by setting it to '0'
-                    area++; // Increase area for each connected land cell
+                if (r >= 0 && r < m && c >= 0 && c < n) {
+                    if (grid[r][c] != 0) {
+                        area++;
+                        q.add((new int[]{r, c}));
+                        grid[r][c] = 0;
+                    }
                 }
             }
         }
-
         return area;
     }
 
