e4ee0b8f3506bbdf9c21f6e61c4a46c0040a4f86
404FoundU <unnikrishnanrp@outlook.com>
2025-01-11 12:10:42 -0600

    stream

--- src/Functional/Examples/StreamExample.java
+++ src/Functional/Examples/StreamExample.java
@@ -2,7 +2,10 @@ package Functional.Examples;
 
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class StreamExample {
@@ -18,8 +21,69 @@ public class StreamExample {
         empList.add(new Employee(107, "srinu", 104, "active", 3500));
         //print emp details based on dept
 
+        Map<Integer, List<Employee>> depMap = empList.stream()
+                .collect(Collectors.groupingBy(e -> e.getDeptId(), Collectors.toList()));
+
+     /*   depMap.forEach((k,v)->{
+            System.out.println("k = " + k);
+            v.forEach(System.out::println);
+                }
+        );
+
+
+        depMap.entrySet()
+                .forEach(entry -> {
+                    System.out.println("Department ID: " + entry.getKey());
+                    entry.getValue().forEach(System.out::println);
+                });*/
+
+        // print employee count in each dept
+        Map<Integer, Long> deptCount = empList.stream()
+                .collect(Collectors.groupingBy(e -> e.getDeptId(), Collectors.counting()));
+/*        deptCount.forEach((k,v)->{
+                    System.out.println("k = " + k);
+            System.out.println("v = " + v);
+                }
+        )*/
+        ;
+
+        // active and inactive employees in the collection
+        Map<String, List<Employee>> activeMap = empList.stream()
+                .collect(Collectors.groupingBy(e -> e.getStatus(), Collectors.toList()));
+        activeMap.entrySet()
+                .forEach(
+                        (entry) -> {
+                            System.out.println("entry.getKey() = " + entry.getKey());
+                            entry.getValue().forEach(System.out::println);
+                        }
+                );
+
+        // count of active and inactive
+        long activeCount = empList.stream()
+                .filter(e -> "active".equals(e.getStatus()))
+                .count();
+
+        // max min employee salary
+
+        Optional<Employee> maxSalary = empList.stream()
+                .max((a, b) -> Integer.compare(a.getSalary(), b.getSalary()));
+
+        maxSalary.ifPresent(s -> System.out.println("salary = " + s));
+
         empList.stream()
-                .collect(Collectors.groupingBy(e -> e.getDeptId(), ))
+                .max(Comparator.comparing(e -> e.getSalary()))
+                .ifPresent(s -> System.out.println("salary = " + s));
+        //max salary of an employee for each dept
+
+        Map<Integer, Optional<Employee>> salaryMap = empList.stream()
+                .collect(Collectors.groupingBy(e -> e.getDeptId(),
+                        Collectors.maxBy(Comparator.comparing(e -> e.getSalary()))));
+
+        salaryMap.forEach((k, v) -> {
+            System.out.println("k = " + k);
+            v.ifPresent(e -> System.out.println("salary = " + e.getSalary()));
+        });
+
 
     }
 
@@ -81,6 +145,7 @@ class Employee {
     public void setSalary(int salary) {
         this.salary = salary;
     }
+
 
     // Getters and setters can be added here for access
     @Override
