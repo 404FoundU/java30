596813c5155cd56d70f4791d0ea498595424d0fc
404FoundU <unnikrishnanrp@outlook.com>
2025-01-13 11:54:36 -0600

    stream

--- src/DSA/Graph/bfs/WatchedVideosByFriends.java
+++ src/DSA/Graph/bfs/WatchedVideosByFriends.java
@@ -12,47 +12,53 @@ import java.util.stream.Collectors;
 //https://leetcode.com/problems/get-watched-videos-by-your-friends
 public class WatchedVideosByFriends {
 
-    public List<String> watchedVideosByFriends(List<List<String>> watchedVideos, int[][] friends, int id, int level) {
+    public List<String> watchedVideosByFriends(List<List<String>> watchedVideos, int[][] graph, int source, int level) {
+        int n = graph.length;
 
-        int n = friends.length;
         boolean[] visited = new boolean[n];
         Queue<Integer> q = new LinkedList<>();
-        q.add(id);
-        visited[id] = true;
-        int currentLevel = 0;
+        q.add(source);
+        visited[source] = true;
 
+        int currentLevel = 0;
         while (!q.isEmpty()) {
-            if (currentLevel >= level) {
+            int size = q.size();
+            // navigate to current level friends
+            if (level == currentLevel) {
                 break;
             }
-
-            int size = q.size();
             for (int i = 0; i < size; i++) {
-                int currentPerson = q.poll();
-                for (int friend : friends[currentPerson]) {
-                    if (!visited[friend]) {
-                        q.add(friend);
-                        visited[friend] = true;
+                int current = q.poll();
+
+                for (int neighbour : graph[current]) {
+                    if (!visited[neighbour]) {
+                        visited[neighbour] = true;
+                        q.add(neighbour);
                     }
                 }
             }
             currentLevel++;
+
         }
 
-        Map<String, Integer> videoAndCount = new HashMap<>();
+        Map<String, Integer> freqCounter = new HashMap<>();
+        // queue has only friends
         while (!q.isEmpty()) {
-            int person = q.poll();
-            for (String video : watchedVideos.get(person)) {
-                videoAndCount.put(video, videoAndCount.getOrDefault(video, 0) + 1);
-            }
+            int current = q.poll();
+            List<String> videos = watchedVideos.get(current);
+            videos.forEach(video -> {
+                Integer count = freqCounter.getOrDefault(video, 0);
+                freqCounter.put(video, count + 1);
+            });
         }
 
-
-        return videoAndCount.entrySet()
+        return freqCounter.entrySet()
                 .stream()
-                .sorted((a, b) -> a.getValue().compareTo(b.getValue())) // Sort by values
-                .map(Map.Entry::getKey)              // Extract the keys
+                .sorted((a, b) -> a.getValue().compareTo(b.getValue()))
+                .map(f -> f.getKey())
                 .collect(Collectors.toList());
+
+
     }
 
 
