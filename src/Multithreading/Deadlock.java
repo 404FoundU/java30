fe6ca90ffe4f0b213d0cf1d4e11febab9e0cee1b
404FoundU <unnikrishnanrp@outlook.com>
2025-01-24 12:26:38 -0600

    deadlock

--- src/Multithreading/Deadlock.java
+++ src/Multithreading/Deadlock.java
@@ -8,44 +8,60 @@ public class Deadlock {
     public static void main(String[] args) {
         System.out.println("Started main thread");
 
-        Object lock1 = "unni";
-        Object lock2 = "rishi";
+        // Two locks
+        Object lock1 = "Lock1";
+        Object lock2 = "Lock2";
 
-        Runnable runnable = () -> {
+        // Thread 1 tries to acquire lock1 first, then lock2
+        Thread thread1 = new Thread(() -> {
             synchronized (lock1) {
+                System.out.println(Thread.currentThread().getName() + " acquired lock1");
                 try {
-                    Thread.sleep(10); // wont relinquish the lock
-
+                    // Simulate work with lock1
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                System.out.println(Thread.currentThread().getName() + " waiting to acquire lock2");
                 synchronized (lock2) {
-                    System.out.println("lock acquired 1");
+                    System.out.println(Thread.currentThread().getName() + " acquired lock2");
                 }
             }
-        };
-        Thread thread1 = new Thread(runnable, "Thread1");
+        }, "Thread1");
 
+        // Thread 2 tries to acquire lock2 first, then lock1
         Thread thread2 = new Thread(() -> {
             synchronized (lock2) {
+                System.out.println(Thread.currentThread().getName() + " acquired lock2");
                 try {
-                    Thread.sleep(10);
+                    // Simulate work with lock2
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                System.out.println(Thread.currentThread().getName() + " waiting to acquire lock1");
                 synchronized (lock1) {
-                    System.out.println("lock acquired for " + Thread.currentThread().getName());
+                    System.out.println(Thread.currentThread().getName() + " acquired lock1");
                 }
             }
         }, "Thread2");
 
+        // Start both threads
         thread1.start();
-        System.out.println(thread1.getState().name());
         thread2.start();
-        System.out.println(thread1.getState().name());
-        System.out.println("exit main thread");
 
+        // Monitor threads' states
+        new Thread(() -> {
+            while (true) {
+                System.out.println("Thread1 State: " + thread1.getState());
+                System.out.println("Thread2 State: " + thread2.getState());
+                try {
+                    Thread.sleep(1000); // Check states periodically
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
-
-
 }
+
