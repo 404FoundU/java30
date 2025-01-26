c5037c5f1b1919018c989cb1beff752ca5771b04
404FoundU <unnikrishnanrp@outlook.com>
2025-01-25 19:02:24 -0600

    failfast

---
+++ src/main/java/DesignPattern/IteratorPatternExample.java
@@ -0,0 +1,71 @@
+package DesignPattern;
+
+// Iterator Interface
+interface Iterator<T> {
+    boolean hasNext();
+
+    T next();
+}
+
+// Aggregate Interface
+interface Aggregate<T> {
+    Iterator<T> createIterator();
+}
+
+// Concrete Aggregate
+class PlaylistCollection implements Aggregate<String> {
+    private String[] songs;  // Array of songs
+
+    public PlaylistCollection(String[] songs) {
+        this.songs = songs;
+    }
+
+    @Override
+    public Iterator<String> createIterator() {
+        return new PlaylistIterator(songs);  // Return a PlaylistIterator
+    }
+}
+
+
+// Concrete Iterator
+class PlaylistIterator implements Iterator<String> {
+    private String[] songs;
+    private int position = 0;  // Keep track of the current position
+
+    public PlaylistIterator(String[] songs) {
+        this.songs = songs;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position < songs.length;  // Check if more songs exist
+    }
+
+    @Override
+    public String next() {
+        if (hasNext()) {
+            return songs[position++];  // Return the current song and move to the next
+        }
+        return null;  // If no more songs, return null
+    }
+}
+
+
+// Client Code
+public class IteratorPatternExample {
+    public static void main(String[] args) {
+        // Create a playlist of songs
+        String[] songs = {"Song A", "Song B", "Song C", "Song D"};
+        PlaylistCollection playlistCollection = new PlaylistCollection(songs);
+
+        // Get an iterator for the playlist
+        Iterator<String> iterator = playlistCollection.createIterator();
+
+        // Use the iterator to navigate through the playlist
+        System.out.println("Playlist:");
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
+
