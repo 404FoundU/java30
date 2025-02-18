8da1378acf90034993c20e70f7fbca17be24c39c
404FoundU <unnikrishnanrp@outlook.com>
2025-02-17 21:10:31 -0600

    howSUm

---
+++ src/main/java/DSA/Dynamic/BestSum.java
@@ -0,0 +1,79 @@
+package DSA.Dynamic;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// return null if you cant make targetSum
+public class BestSum {
+
+    public static void main(String[] args) {
+        System.out.println(bestSum(7, new int[]{5, 3, 4, 7}));
+    }
+
+    private static List<Integer> bestSum(int targetSum, int[] nums) {
+        if (targetSum == 0) {
+            return new ArrayList<>();
+        }
+
+        if (targetSum < 0) {
+            return null;
+        }
+        List<Integer> shortestCombination = null;
+        for (int num : nums) {
+            int newTargetSum = targetSum - num;
+            List<Integer> integerList = bestSum(newTargetSum, nums);
+            if (integerList != null) {
+                List<Integer> sumList = new ArrayList<>(integerList);
+                sumList.add(num);
+                if (shortestCombination == null || shortestCombination.size() > sumList.size()) {
+                    shortestCombination = new ArrayList<>(sumList);
+                }
+            }
+        }
+        return shortestCombination;
+    }
+
+    private static final Map<Integer, List<Integer>> memo = new HashMap<>();
+
+    private static List<Integer> bestSumMemo(int targetSum, int[] nums) {
+        if (targetSum == 0) {
+            return new ArrayList<>();
+        }
+        if (targetSum < 0) {
+            return null;
+        }
+
+        if (memo.containsKey(targetSum)) {
+            memo.get(targetSum);
+        }
+        List<Integer> shortestCombination = null;
+        for (int num : nums) {
+            int newTargetSum = targetSum - num;
+            List<Integer> integerList = bestSum(newTargetSum, nums);
+            if (integerList != null) {
+                List<Integer> sumList = new ArrayList<>(integerList);
+                sumList.add(num);
+                if (shortestCombination == null || shortestCombination.size() > sumList.size()) {
+                    shortestCombination = new ArrayList<>(sumList);
+                    memo.put(newTargetSum, shortestCombination);
+                }
+            }
+        }
+        memo.put(targetSum, shortestCombination);
+        return shortestCombination;
+    }
+    /*
+    complexity
+    if m is the target sum
+    depth will be -1(worst case) on each level. So depth = m
+    width is the number of elements in nums array n
+    so, n^m
+
+space:
+depth = n
+recursion does not create new arrays. O(1)
+total = O(n)
+     */
+}
