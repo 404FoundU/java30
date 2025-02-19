f5ce331e89f1b3676a5a1aae028f432b6a17cc8f
404FoundU <unnikrishnanrp@outlook.com>
2025-02-19 11:17:57 -0600

    jsonConvert

---
+++ src/main/java/DSA/Arrays/Strings/GetExpressionString.java
@@ -0,0 +1,54 @@
+package DSA.Arrays.Strings;
+
+public class GetExpressionString {
+
+    static String getExpressionString(String exp) {
+
+
+        char[] charArray = exp.toCharArray();
+        StringBuilder result = new StringBuilder();
+        result.append("BigDecimal result = ");
+        int counter = 0;
+
+        for (char c : charArray) {
+            if (Character.isLetterOrDigit(c)) {
+                result.append("BigDecimal.valueOf(").append(c).append(")");
+            } else if (c == '+') {
+                result.append(".add(");
+                counter++;
+            } else if (c == '-') {
+                result.append(".subtract(");
+                counter++;
+            } else if (c == '*') {
+                result.append(".multiply(");
+                counter++;
+            } else if (c == '/') {
+                result.append(".divide(");
+                counter++;
+            } else if (c == '(' || c == ')') {
+                result.append(c);
+            } else if (c == ' ') {
+                continue;
+            }
+        }
+
+        while (counter > 0) {
+            result.append(")");
+            counter--;
+        }
+
+        result.append(";");
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println(getExpressionString("x+(y/z)+400-x)"));
+        // x + (y / z) + 400 - x
+        //"BigDecimal result = BigDecimal.valueOf(x).add((BigDecimal.valueOf(y).
+        //divide(BigDecimal.valueOf(z))).add(BigDecimal.valueOf(400)).subtract
+        //(BigDecimal.valueOf(x)));"
+    }
+
+}
--- src/main/java/DSA/Arrays/Strings/JsonConvert.java
+++ src/main/java/DSA/Arrays/Strings/JsonConvert.java
@@ -1,4 +1,4 @@
-package DSA.Arrays;
+package DSA.Arrays.Strings;
 
 
 import java.util.List;
