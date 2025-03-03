5772111afceef8c9d40d33115dd5a4df2bc68ab2
404FoundU <unnikrishnanrp@outlook.com>
2025-03-03 16:27:03 -0600

    dynamic

--- src/main/java/DSA/Dynamic/MaxSumContiguousArrayKadens.java
+++ src/main/java/DSA/Dynamic/MaxSumContiguousArrayKadens.java
@@ -1,6 +1,7 @@
 package DSA.Dynamic;
 
 //https://www.youtube.com/watch?v=hLPkqd60-28
+// bottom up
 public class MaxSumContiguousArrayKadens {
     public static void main(String[] args) {
         MaxSumContiguousArrayKadens test = new MaxSumContiguousArrayKadens();
@@ -13,15 +14,15 @@ public class MaxSumContiguousArrayKadens {
     }
 
     private int testMethod(int[] nums) {
+        int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
-        int currentSum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            currentSum += nums[i];
-            maxSum = Math.max(maxSum, currentSum);
-            if (currentSum < 0) {
-                currentSum = 0;
-            }
 
+        for (int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+            maxSum = Math.max(currSum, maxSum);
+            if (currSum < 0) {
+                currSum = 0;
+            }
         }
         return maxSum;
 
@@ -42,6 +43,27 @@ public class MaxSumContiguousArrayKadens {
         return maxSum;
 
     }
+
+    /*
+    Steps: calculate max sum at each level
+    i=0
+    -2
+    -2  -3
+    -2  -3 + 4
+    -2  -3 + 4  -1
+    -2  -3 + 4  -1 -2
+    -2  -3 + 4  -1 -2 + 1
+    -2  -3 + 4  -1 -2 + 1 + 5
+    -2  -3 + 4  -1 -2 + 1 + 5 - 3
+    i=1
+    -3
+    -3 + 4
+    -3 + 4  -1
+    -3 + 4  -1 -2
+    -3 + 4  -1 -2 + 1
+    -3 + 4  -1 -2 + 1 + 5
+    -3 + 4  -1 -2 + 1 + 5 - 3
+     */
 
 
 }
