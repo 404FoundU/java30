6f9c0419d7af24a3ce54b6f395d0bccfbd2a0fdb
404FoundU <unnikrishnanrp@outlook.com>
2025-03-10 11:24:11 -0500

    boyer moore

--- src/main/java/DSA/HashTable/MajorityElement.java
+++ src/main/java/DSA/HashTable/MajorityElement.java
@@ -1,26 +1,33 @@
 package DSA.HashTable;
 
 //https://leetcode.com/problems/majority-element/description/
+// Boyer Moore Voting algorithm
 public class MajorityElement {
     public static int majorityElement(int[] nums) {
-        int candidate = nums[0];
+        int element = nums[0];
         int count = 0;
-
         for (int num : nums) {
             if (count == 0) {
-                candidate = num;
+                element = num;
             }
-            count += (num == candidate) ? 1 : -1;
+            if (element == num) {
+                count++;
+            } else {
+                count--;
+            }
         }
 
-        return candidate;
+        return element;
+
     }
 
     public static void main(String[] args) {
         int[] nums1 = {3, 2, 3};
         int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
+        int[] nums3 = {3, 2, 1, 1, 2, 2, 2, 3, 2};
 
         System.out.println("Majority Element: " + majorityElement(nums1)); // Output: 3
         System.out.println("Majority Element: " + majorityElement(nums2)); // Output: 2
+        System.out.println("Majority Element: " + majorityElement(nums3)); // Output: 2
     }
 }
