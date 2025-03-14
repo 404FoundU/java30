9ec55c0c77843d78801cd5307f462b0246291a42
404FoundU <unnikrishnanrp@outlook.com>
2025-03-14 12:19:36 -0500

    monotonic

--- src/main/java/DSA/Stack/MonotonicStack/DailyTemperatures.java
+++ src/main/java/DSA/Stack/MonotonicStack/DailyTemperatures.java
@@ -6,19 +6,22 @@ import java.util.Stack;
 //https://www.youtube.com/watch?v=_ZEvmycwXHs
 class DailyTemperatures {
     public int[] dailyTemperatures(int[] temperatures) {
-        int n = temperatures.length;
-        int[] result = new int[n];
+
+        int[] nextGreaterTemp = new int[temperatures.length];
+        Arrays.fill(nextGreaterTemp, 0); // no required as int default is 0
         Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
-                int prevIndex = stack.pop();
-                result[prevIndex] = i - prevIndex;
+        //monotonic decreasing
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                Integer previousDay = stack.pop();
+                // next greater temp is at temperatures[i] which is ith position
+                // calculate the numbere of days
+                nextGreaterTemp[previousDay] = i - previousDay;
             }
             stack.push(i);
         }
-
-        return result;
+        return nextGreaterTemp;
     }
 
     public static void main(String[] args) {
