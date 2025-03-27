a7f258ce95e1c86ff463109933e250a358757b58
404FoundU <unnikrishnanrp@outlook.com>
2025-03-27 11:04:07 -0500

    maxConsecutiveSequence

--- src/main/java/DSA/HashTable/LongestConsecutiveSequence.java
+++ src/main/java/DSA/HashTable/LongestConsecutiveSequence.java
@@ -2,44 +2,46 @@ package DSA.HashTable;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
+//https://www.youtube.com/watch?v=joIEdeOGqjQ
 public class LongestConsecutiveSequence {
-    public static int longestConsecutive(int[] nums) {
+    public static int longestConsecutive2(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
 
-        HashSet<Integer> numSet = new HashSet<>();
-        for (int num : nums) {
-            numSet.add(num);
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
         }
 
-        int longestStreak = 0;
+        int maxLength = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // check left
+            int leftElement = nums[i] - 1;
+            int currentStreak = 1;
 
-        for (int num : numSet) {
-            // Check if num is the start of a sequence
-            if (!numSet.contains(num - 1)) {
-                int currentNum = num;
-                int currentStreak = 1;
-
-                while (numSet.contains(currentNum + 1)) {
-                    currentNum++;
+            if (!set.contains(leftElement)) {
+                int nextElement = nums[i] + 1;
+                while (set.contains(nextElement)) {
+                    nextElement++;
                     currentStreak++;
                 }
-
-                longestStreak = Math.max(longestStreak, currentStreak);
+                maxLength = Math.max(currentStreak, maxLength);
             }
+
         }
 
-        return longestStreak;
+        return maxLength;
     }
 
     public static void main(String[] args) {
         int[] nums1 = {100, 4, 200, 1, 3, 2};
 //        System.out.println("Longest consecutive sequence length: " + longestConsecutive(nums1)); // Output: 4
-        System.out.println("Longest consecutive sequence length: " + longestConsecutiveBruteForce(nums1)); // Output: 4
+        System.out.println("Longest consecutive sequence length: " + longestConsecutive2(nums1)); // Output: 4
 
         int[] nums2 = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
 //        System.out.println("Longest consecutive sequence length: " + longestConsecutive(nums2)); // Output: 9
-        System.out.println("Longest consecutive sequence length: " + longestConsecutiveBruteForce(nums2)); // Output: 9
+        System.out.println("Longest consecutive sequence length: " + longestConsecutive2(nums2)); // Output: 9
     }
 
     public static int longestConsecutiveBruteForce(int[] nums) {
