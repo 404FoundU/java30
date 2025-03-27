60772e88e7540d74b213d842dec270dff1517c35
404FoundU <unnikrishnanrp@outlook.com>
2025-03-27 15:29:25 -0500

    longestArithmeticSequence

--- src/main/java/DSA/HashTable/LongestArithmeticSubsequence.java
+++ src/main/java/DSA/HashTable/LongestArithmeticSubsequence.java
@@ -1,6 +1,44 @@
 package DSA.HashTable;
 
+import java.util.HashMap;
+import java.util.Map;
+
+//https://www.youtube.com/watch?v=_xIkHvSZpDE
 public class LongestArithmeticSubsequence {
+
+
+    public static int longestArithSeqLength(int[] nums) {
+        int n = nums.length;
+        if (n <= 1) return 0;
+        Map<Integer, Integer>[] dp = new HashMap[n];
+        Integer[] intArray = new Integer[]{};
+        int maxLength = 2;
+
+        for (int i = 0; i < n; i++) {
+
+            dp[i] = new HashMap<>();
+            for (int j = 0; j < i; j++) {
+                // diff of all the elements wrt i
+                int diff = nums[j] - nums[i];
+
+                int existingLength;
+                Map<Integer, Integer> diffMap = dp[j];
+                //does the same difference exist in the previous map. Increment it by 1
+                if (diffMap.containsKey(diff)) {
+                    existingLength = diffMap.get(diff);
+                } else {
+                    existingLength = 1;
+                }
+
+                int calculatedLength = existingLength + 1;
+                dp[i].put(diff, calculatedLength);
+                maxLength = Math.max(calculatedLength, maxLength);
+
+            }
+        }
+        return maxLength;
+    }
+
     public static int longestArithSeqLengthBF(int[] nums) {
         int n = nums.length;
         if (n <= 1) return 0;
@@ -10,6 +48,7 @@ public class LongestArithmeticSubsequence {
             for (int j = i + 1; j < n; j++) {
                 int diff = nums[j] - nums[i];
                 int currLength = 2;
+
                 int prev = nums[j];
                 for (int k = j + 1; k < n; k++) {
 
@@ -30,8 +69,8 @@ public class LongestArithmeticSubsequence {
         int[] nums2 = {9, 4, 7, 2, 10};
         int[] nums3 = {20, 1, 15, 3, 10, 5, 8};
 
-        System.out.println("Example 1: " + longestArithSeqLengthBF(nums1)); // 4
-        System.out.println("Example 2: " + longestArithSeqLengthBF(nums2)); // 3
-        System.out.println("Example 3: " + longestArithSeqLengthBF(nums3)); // 4
+        System.out.println("Example 1: " + longestArithSeqLength(nums1)); // 4
+//        System.out.println("Example 2: " + longestArithSeqLength(nums2)); // 3
+//        System.out.println("Example 3: " + longestArithSeqLength(nums3)); // 4
     }
 }
