405c3bf2edb1d4b24c3d4bb74858908a4ba3c433
404FoundU <unnikrishnanrp@outlook.com>
2025-04-01 17:26:43 -0500

    threeSum

--- src/main/java/DSA/HashTable/ThreeSum.java
+++ src/main/java/DSA/HashTable/ThreeSum.java
@@ -7,6 +7,39 @@ import java.util.List;
 //https://www.youtube.com/watch?v=54VcLqAHrHk
 public class ThreeSum {
 
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate i
+
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Skip duplicate left and right
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+
+                } else if (sum < 0) {
+                    left++; // need a bigger number
+                } else {
+                    right--; // need a smaller number
+                }
+            }
+        }
+
+        return result;
+    }
     public static List<List<Integer>> threeSumBF(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
@@ -33,7 +66,7 @@ public class ThreeSum {
 
     public static void main(String[] args) {
         int[] nums = {-1, 0, 1, 2, -1, -4};
-        List<List<Integer>> result = threeSumBF(nums);
+        List<List<Integer>> result = threeSum(nums);
         System.out.println("Triplets: " + result);
         //Triplets: [[-1, -1, 2], [-1, 0, 1]]
     }
